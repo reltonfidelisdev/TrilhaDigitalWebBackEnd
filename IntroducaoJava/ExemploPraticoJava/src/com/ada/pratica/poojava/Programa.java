9f411eca30491b88d9a5bed61d2d5e827cd04872
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-08 10:25:30 -0300

    update add calls to iterate to the Animals object

--- IntroducaoJava/ExemploPraticoJava/src/com/ada/pratica/poojava/Programa.java
+++ IntroducaoJava/ExemploPraticoJava/src/com/ada/pratica/poojava/Programa.java
@@ -11,8 +11,11 @@ public class Programa {
 		cachorroUm.peso			= 5.5;
 		cachorroUm.tamanhoRabo	= 5;
 		
-		cachorroUm.latir();
-		System.out.println(cachorroUm.pegar());
+		cachorroUm.latir(); // AU AU
+		System.out.printf("%n O cachorro pegou uma %s.", cachorroUm.pegar()); // O cachorro pegou uma Bolinha.
+		System.out.printf("%n O cachorro está %s.", cachorroUm.interagir("carinho")); // O cachorro está feliz.
+		System.out.printf("%n O cachorro está %s.", cachorroUm.interagir("")); // O cachorro está neutro.
+		System.out.printf("%n O cachorro está %s.", cachorroUm.interagir("vai dormir")); //  O cachorro está Bravo.
 	}
 
 }
