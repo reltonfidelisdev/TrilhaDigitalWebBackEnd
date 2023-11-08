f1e20a166b2d6a30fe48bc89d2ee01bd30043283
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-08 17:34:27 -0300

    update to use inheritance and polymorphism of oop progamming language

--- IntroducaoJava/ExemploPraticoJava/src/com/ada/pratica/poojava/Programa.java
+++ IntroducaoJava/ExemploPraticoJava/src/com/ada/pratica/poojava/Programa.java
@@ -1,31 +1,21 @@
 package com.ada.pratica.poojava;
 
+import javax.management.remote.SubjectDelegationPermission;
+
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Animals cachorroUm = new Animals();
-		cachorroUm.setNome("lily");
-		cachorroUm.setCor("Caralemo");
-		cachorroUm.setAltura(5);
-		cachorroUm.setPeso(2.5);
-		cachorroUm.setTamanhoRabo(5);
 		
-		System.out.printf("%n O nome do animal é %s.", cachorroUm.getNome()); // rex
-		System.out.printf("%n O cachorro pegou uma %s.", cachorroUm.pegar()); // O cachorro pegou uma Bolinha.
-		System.out.printf("%n O cachorro está %s.", cachorroUm.interagir("carinho")); // O cachorro está feliz.
-		System.out.printf("%n O cachorro está %s.", cachorroUm.interagir("")); // O cachorro está neutro.
-		System.out.printf("%n O cachorro está %s.", cachorroUm.interagir("vai dormir")); //  O cachorro está Bravo.
-		System.out.printf("%n O cachorro está %s.", cachorroUm.interagir("pisar na patinha")); //  O cachorro está triste.
-		
-		Animals cachorroDois = new Animals("Rex","Branco", 7, 3, 4, "");
-		System.out.printf("%n cachorro#Um: %s",cachorroUm.toString());
-		System.out.println("\n cachorro#Dois: " + cachorroDois.toString());
-		cachorroDois = cachorroUm;
-		// cachorroDois point to cachorroUm
-		// the reference to the cashorroDois is lost
-		// the garbage collector its in action
-		System.out.printf("cachorro#Dois: %s",cachorroDois.toString());
+		Cachorro cachorro = new Cachorro();
+		cachorro.nome = "Rex";
+		cachorro.cor = "Caramelo";
+		cachorro.altura = 10;
+		cachorro.peso = 5.3;
+		cachorro.estadoEspirito = "de boa";
+		System.out.println("{{"+  cachorro.toString() +"}}");
+		cachorro.doEat(); // Sobrescrita do método
+		cachorro.doSound();
+		cachorro.doSleep();
 	}
 
 }
