8658d5a2f09166930ca86f297b16a527b51454aa
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-08 09:46:52 -0300

    create new, define Animals class instance object and assing values.

---
+++ IntroducaoJava/ExemploPraticoJava/src/com/ada/pratica/poojava/Programa.java
@@ -0,0 +1,17 @@
+package com.ada.pratica.poojava;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Animals cachorroUm = new Animals();
+		cachorroUm.nome			= "Puppy";
+		cachorroUm.cor			= "Marron";
+		cachorroUm.altura		= 25;
+		cachorroUm.peso			= 5.5;
+		cachorroUm.tamanhoRabo	= 5;
+		
+		System.out.println(cachorroUm);
+	}
+
+}
