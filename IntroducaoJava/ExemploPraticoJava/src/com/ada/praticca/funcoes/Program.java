f686ca26dafcf3710117e91096aa76592674c745
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-08 01:46:17 -0300

    update functions, add saudacao method that say Hello Relton in to console.

--- IntroducaoJava/ExemploPraticoJava/src/com/ada/praticca/funcoes/Program.java
+++ IntroducaoJava/ExemploPraticoJava/src/com/ada/praticca/funcoes/Program.java
@@ -3,9 +3,10 @@ package com.ada.praticca.funcoes;
 public class Program {
 
 	public static void main(String[] args) {
-
-		
-
+		saudacao();
 	}
 
+	public static void saudacao() {
+		System.out.println("Hello Relton.");
+	}
 }
